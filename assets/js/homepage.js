5a930b86ae39074c4e1b3dd794ae4112f6daecec
Sheroo Bhagia <sheroo@sb-mac.lan>
2022-02-02 00:36:13 -0600

    code from Canvas

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,11 +1,14 @@
-var getUserRepos = function (user) {
-    var apiURL = "https://api.github.com/users/" + user + "/repos"
-    fetch(apiURL).then(function (response) {
-        response.json().then(function (data) {
-            console.log(data);
-        });
+var getUserRepos = function(user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a get request to url
+  fetch(apiUrl).then(function(response) {
+    console.log(response);
+    response.json().then(function(data) {
+      console.log(data);
     });
+  });
+};
 
-}
-
-getUserRepos("microsoft");
+getUserRepos("lernantino");
