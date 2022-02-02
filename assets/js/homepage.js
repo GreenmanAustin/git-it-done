ddaf6d197356b993a64d639ce9a378082093d122
Sheroo Bhagia <sheroo@sb-mac.lan>
2022-02-02 02:07:39 -0600

    added user repos to site

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,14 +1,62 @@
-var getUserRepos = function(user) {
-  // format the github api url
-  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
 
-  // make a get request to url
-  fetch(apiUrl).then(function(response) {
-    console.log(response);
-    response.json().then(function(data) {
-      console.log(data);
-    });
-  });
+
+var displayRepos = function (repos, searchTerm) {
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    };
+    for (var i = 0; i < repos.length; i++) {
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+        repoEl.appendChild(titleEl);
+        var statusEl = document.createElement("span");
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + "issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+        repoEl.appendChild(statusEl);
+        repoContainerEl.appendChild(repoEl);
+    }
+}
+var getUserRepos = function (user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a get request to url
+    fetch(apiUrl).then(function (response) {
+        // console.log(response);
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayRepos(data, user);
+            });
+        } else {
+            alert("Error: GitHub User Not Found");
+        }
+    })
+        .catch(function (error) {
+            alert("Unable to connect to GitHub");
+        });
 };
 
-getUserRepos("lernantino");
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    var userName = nameInputEl.value.trim();
+    if (userName) {
+        getUserRepos(userName);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+}
+
+userFormEl.addEventListener("submit", formSubmitHandler);
